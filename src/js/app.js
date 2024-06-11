9c4649adbf3b3f537a3fb8012ec7aea1a9fb3fa9
Danomolina <danomolina@gmail.com>
2024-06-11 20:04:44 +0000

    Ex_Conditional

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import "../style/index.css";
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+        avatarURL: "https://pbs.twimg.com/profile_images/1541087885/DSC_00951_400x400.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
         
         twitter: null, // social media usernames
@@ -28,19 +28,34 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Pamela";
+  if (variables.lastName == null) variables.lastName = "Godoy";
+  if (variables.role == null) variables.role = "Web Developer";
+  if (variables.country == null) variables.country = "CHILITO";
+  if (variables.city == null) variables.city = "Santiago";
+
+  if (variables.twitter == null) variables.twitter = "4geeksacademy";
+  if (variables.github == null) variables.github = "4geeksacademy";
+  if (variables.linkedin == null) variables.linkedin = "4geeksacademy";
+  if (variables.instagram == null) variables.instagram = "4geeksacademy";
+
+  /*   <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+  <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+  <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li> */
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
